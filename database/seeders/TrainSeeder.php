aa057c4d56075a45dd10fef320e6b4b8ab8a4f75
DylanBlanco <dylanblanco1198@gmail.com>
2024-10-23 18:39:18 +0200

    inserito dati con seeder tramite fake

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// MODELS
+use App\Models\Train;
 class TrainSeeder extends Seeder
 {
     /**
@@ -12,6 +14,18 @@ class TrainSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+            $train->code = strtolower(fake()->shuffle(fake()->bothify('????####')));
+            $train->company = fake()->company();
+            $train->dep_station = fake()->city();
+            $train->arr_station = fake()->city();
+            $train->dep_time = fake()->time();
+            $train->arr_time = fake()->time();
+            $train->carriages_number = rand(2, 12);
+            $train->on_time = fake()->boolean();
+            $train->canceled = fake()->boolean();
+            $train->save();
+        }
     }
 }
